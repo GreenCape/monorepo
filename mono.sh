bacbb54dc5087375de3eb854438d0458db18df33
Niels Braczek <nbraczek@bsds.de>
2021-12-11 14:20:33 +0100

    Cleanup after accident

--- mono.sh
+++ mono.sh
@@ -1,56 +1,48 @@
 #!/usr/bin/env bash
 
-MONO_FULL_NAME="MonoRepo Tool"
-MONO_VERSION="0.0.1"
-MONO_AUTHOR="GreenCape, Niels Braczek"
-MONO_COPYRIGHT_YEAR="2021"
-MONO_PLUGIN_DIR=${MONO_PLUGIN_DIR:-./plugins}
-
-function hello {
-  echo "$MONO_FULL_NAME $MONO_VERSION by $MONO_AUTHOR"
-  echo ""
-}
-
-function version {
-  echo "$MONO_FULL_NAME $MONO_VERSION"
-  echo "(C)$MONO_COPYRIGHT_YEAR $MONO_AUTHOR"
-}
-
-function usage {
-  hello
-  cat <<EOT
-Usage: mono [-h|--help] [-v|--version]
-   or: mono <command> [<arguments>]
-
-'mono help -a' and 'mono help -g' list available commands and some
-instructions on mono concepts respectively. Use 'mono help <command>'
-or 'mono help <concept>' to learn more about a specific command or concept.
-EOT
-}
-
-case $1 in
-  -h | --help)
-    usage
-    exit 0
-    ;;
-  -v | --version)
-    version
-    exit 0
-    ;;
-  *)
-    MONO_COMMAND=$1
-    ;;
-esac
-
+MONO_PLUGIN_DIR=./plugins
+MONO_COMMAND=$1
 MONO_PLUGIN_FILE="$MONO_PLUGIN_DIR/mono-$MONO_COMMAND.sh"
 
-if [ ! -f "$MONO_PLUGIN_FILE" ]; then
-  # Plugin does not exist
-  echo "Command »$MONO_COMMAND« not found";
-  usage
-  exit 1
-fi
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
 
-hello
-shift
-source "$MONO_PLUGIN_FILE"
+  case $key in
+  -e | --extension)
+    EXTENSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -e=* | --extension=*)
+    EXTENSION="${i#*=}"
+    shift # past argument=value
+    ;;
+  -s | --searchpath)
+    SEARCHPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -s=* | --searchpath=*)
+    SEARCHPATH="${i#*=}"
+    shift # past argument=value
+    ;;
+  -l | --lib)
+    LIBPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+  -l=* | --lib=*)
+    LIBPATH="${i#*=}"
+    shift # past argument=value
+    ;;
+  --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+  *) # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift              # past argument
+    ;;
+  esac
+done
